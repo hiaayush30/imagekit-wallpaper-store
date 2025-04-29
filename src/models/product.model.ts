d3c039f0e37ed713a6b6468117e717e69c7ffca2
aayush <hiaayush30@gmail.com>
2025-04-30 02:21:45 +0530

    product  schema

---
+++ src/models/product.model.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const imageVariantSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ["SQUARE", "WIDE", "POTRAIT"],
+        required: true
+    },
+    price: {
+        type: Number,
+        min: 0
+    },
+    lisences: {
+        type: String,
+        enum: ["personal", "commercial"]
+    }
+}, { timestamps: true })
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    variants: [imageVariantSchema]
+}, { timestamps: true })
+
+const Product = mongoose.models['Product'] ? mongoose.models['Product'] : mongoose.model<imageVariantSchema>('Product', productSchema);
+
+export default Product;
